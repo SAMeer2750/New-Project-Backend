48802264d2dc858d0b2b2ab186196d326fa50ab1
Your Name <you@example.com>
2024-02-02 00:44:29 +0530

    updated nft contract to get owners tokens

--- src/MainEngein.sol
+++ src/MainEngein.sol
@@ -1,2 +1,101 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.7;
+
+import {ModelNFTs} from "./NFT.sol";
+
+contract ModelManager {
+    modifier ONLY_Manufacturer() {
+        require(addressToUser[msg.sender].IfManufacturer);
+        _;
+    }
+
+    //user
+    struct user {
+        string UserCid;
+        bool IfManufacturer;
+    }
+
+    struct modelStruct {
+        address contractAdd;
+        string modelName;
+        string modelNum;
+    }
+
+    mapping(address => user) public addressToUser;
+
+    //Models
+    mapping(address => mapping(string => address)) public userModelAdd; // manufacturer to mapp od modelno. to  contract add
+    mapping(address => modelStruct[]) public usersToModel;
+
+    function isManufacturer(address _add) public view returns (bool) {
+        return (addressToUser[_add].IfManufacturer);
+    }
+
+    function createUser(bool _IfManufacturer, string memory _UserCid) public {
+        addressToUser[msg.sender] = user(_UserCid, _IfManufacturer);
+    }
+
+    function addModel(
+        string memory _ModelName,
+        string memory _ModelNum
+    ) public ONLY_Manufacturer {
+        ModelNFTs model = new ModelNFTs(_ModelName, _ModelNum);
+        userModelAdd[msg.sender][_ModelNum] = address(model);
+        usersToModel[msg.sender].push(
+            modelStruct(address(model), _ModelName, _ModelNum)
+        );
+    }
+
+    function mintNFT(
+        string memory _ModelNum,
+        string memory _tokenURI
+    ) public ONLY_Manufacturer {
+        require(
+            userModelAdd[msg.sender][_ModelNum] != address(0),
+            "Model does not exist"
+        );
+        ModelNFTs modelNFT = ModelNFTs(userModelAdd[msg.sender][_ModelNum]);
+        modelNFT.mint(msg.sender, _tokenURI);
+    }
+
+    function batchMintNFTs(
+        string memory _ModelNum,
+        uint256 _totalNum,
+        string memory _tokenURI
+    ) public ONLY_Manufacturer {
+        require(
+            userModelAdd[msg.sender][_ModelNum] != address(0),
+            "Model does not exist"
+        );
+        ModelNFTs modelNFT = ModelNFTs(userModelAdd[msg.sender][_ModelNum]);
+        modelNFT.batchMint(msg.sender, _totalNum, _tokenURI);
+    }
+
+    function transferNFT(
+        string memory _ModelNum,
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) public ONLY_Manufacturer {
+        require(
+            userModelAdd[msg.sender][_ModelNum] != address(0),
+            "Model does not exist"
+        );
+        ModelNFTs modelNFT = ModelNFTs(userModelAdd[msg.sender][_ModelNum]);
+        modelNFT.transferNFT(_from, _to, _tokenId);
+    }
+
+    function batchTransferNFTs(
+        string memory _ModelNum,
+        address _from,
+        address _to,
+        uint256[] memory _tokenIds
+    ) public ONLY_Manufacturer {
+        require(
+            userModelAdd[msg.sender][_ModelNum] != address(0),
+            "Model does not exist"
+        );
+        ModelNFTs modelNFT = ModelNFTs(userModelAdd[msg.sender][_ModelNum]);
+        modelNFT.batchTransferNFTs(_from, _to, _tokenIds);
+    }
+}
--- src/NFT.sol
+++ src/NFT.sol
@@ -2,33 +2,48 @@
 pragma solidity ^0.8.13;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract ModelNFTs is ERC721{
+contract ModelNFTs is ERC721URIStorage{
 
-    constructor(string memory _ModelName, string memory _ModelNum) ERC721(_ModelName, _ModelNum){}
+    constructor(string memory _ModelName, string memory _ModelNum) ERC721(_ModelName, _ModelNum) {}
 
-    uint256 tokenId;
+    mapping (address => uint256[]) public addressToTokenIds ;
 
-    function mint(address _to) external {
+    uint256 tokenId = 1;
+
+    function mint(address _to, string memory _tokenURI) external {
         _safeMint(_to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        addressToTokenIds[_to].push(tokenId);
         tokenId ++;
     }
 
-    function batchMint(address _to, uint256 _totalNum) external {
+    function batchMint(address _to, uint256 _totalNum, string memory _tokenURI) external {
         for (uint256 i = 0; i < _totalNum; i++) {
             _safeMint(_to, tokenId);
+            _setTokenURI(tokenId, _tokenURI);
+            addressToTokenIds[_to].push(tokenId);
             tokenId ++;
         }
     }
 
-    function transferNFT(address _from, address _to, uint256 _tokenId) external{
+    function transferNFT(address _from, address _to, uint256 _tokenId) public{
         _transfer(_from, _to, _tokenId);
+        for (uint256 i = 0; i < addressToTokenIds[_from].length; i++) {
+            if (addressToTokenIds[_from][i] == _tokenId) {
+                addressToTokenIds[_from][i] = addressToTokenIds[_from][addressToTokenIds[_from].length - 1];
+                addressToTokenIds[_from].pop();
+                break;
+            }
+        }
+        addressToTokenIds[_to].push(_tokenId);
     }
 
     function batchTransferNFTs(address _from, address _to, uint256[] memory _tokenIds) external {
         for (uint256 i = 0; i < _tokenIds.length; i++) {
             uint256 tokenIdToTransfer = _tokenIds[i];
-            _transfer(_from, _to, tokenIdToTransfer);
+            transferNFT(_from, _to, tokenIdToTransfer);
         }
     }
 
@@ -38,6 +53,10 @@ contract ModelNFTs is ERC721{
 
     function getTotalSupply() external view returns(uint256){
         return tokenId-1;
+    }
+
+    function getOwnersTokens() external view returns(uint256 [] memory){
+        return addressToTokenIds[msg.sender];
     }
 
 }
